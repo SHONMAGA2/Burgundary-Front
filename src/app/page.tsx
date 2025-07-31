2a54628759a63ea691e601f8dd8c98d7c27bdd25
SHONMAGA2 <ivyne2@gmail.com>
2025-07-31 23:39:28 +0000

    Fixed routing error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function login(){
 return(
 <>
 <h1>Login</h1>
-<form action="https://burgundary-api-8dpz.onrender.com/api/login" method="POST">
+<form action="https://burgundary-api-8dpz.onrender.com/api/SignUp" method="POST">
 <input type="text" name="name" placeholder="enter your name"/>
 <input type="email" name="email" placeholder="enter your email"/>
 <input type="password" name="password" placeholder="enter your password"/>
