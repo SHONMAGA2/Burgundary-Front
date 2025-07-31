1e7b698d12eb7b1dd0d0d361f80192ddf98d22c7
SHONMAGA2 <ivyne2@gmail.com>
2025-07-31 22:33:13 +0000

    Fixed xlosing tag issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ return(
 <>
 <h1>Login</h1>
 <form action="https://burgundary-api-8dpz.onrender.com/API/login" method="POST">
-<input type="text" name="name" placeholder="enter your name">
-<input type="email" name="email" placeholder="enter your email">
-<input type="password" name="password" placeholder="enter your password">
+<input type="text" name="name" placeholder="enter your name"/>
+<input type="email" name="email" placeholder="enter your email"/>
+<input type="password" name="password" placeholder="enter your password"/>
 </form>
 </>
