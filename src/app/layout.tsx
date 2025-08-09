5d028240716f670ee521d08dfb8a9dfeebcb0a52
SHONMAGA2 <ivyne2@gmail.com>
2025-08-09 22:01:20 +0000

    data is now shoen cleanly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,31 @@
 
 import React from 'react';
 
+async function Data(){
+try{
+const response = await fetch("https://burgundary-api-8dpz.onrender.com/api/SignUp");
+
+if(!response.ok) throw new Error(`HTTP Error could not retrieve data: ${response.status}`);
+
+const data = response.json();
+const cleanData = JSON.stringify(data,null,2);
+return cleanData;
+
+}catch(error){
+console.error("Something went wrong",error.message);
+}
+}
+
 export default function homeLayout({children}:{children:React.ReactNode}){
 return(
 <>
 <div className="p-4">
-
 {children}
+
+<div className = " display">
+
+</div>
+{cleanData}
 </div>
 </>
 )
