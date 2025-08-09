affc792b8936849c0e9e97aa464a5b8de288c970
SHONMAGA2 <ivyne2@gmail.com>
2025-08-09 22:32:32 +0000

    made data handling a lot cleaner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,36 @@
 "use client";
 
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 
+const [cleanData,setCleanData] = useState<string>("fetching data...");
+
+useEffect(() => {
 async function Data(){
 try{
 const response = await fetch("https://burgundary-api-8dpz.onrender.com/api/SignUp");
 
 if(!response.ok) throw new Error(`HTTP Error could not retrieve data: ${response.status}`);
 
-const data = response.json();
+const data = await response.json();
 const cleanData = JSON.stringify(data,null,2);
-return cleanData;
+
+setCleanData(cleanData);
+
 
 }catch(error){
 console.error("Something went wrong",error.message);
+setCleanData("Could not fetch data +_+");
 }
 }
+
+Data()
+},[]);
 
 export default function homeLayout({children}:{children:React.ReactNode}){
 return(
 <>
-Data()
 <div className="p-4">
+
 {children}
 
 <div className = " display">
