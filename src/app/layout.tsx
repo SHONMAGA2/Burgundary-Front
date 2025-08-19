7e78ff4119b61baf090c360dd3a07adc59764e71
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-08-19 20:31:38 +0200

    Update layout.tsx fixed errors again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,19 @@ import Link from 'next/link';
 export default function HomeLayout({children}:{children:React.ReactNode}){
 
 return(
-<>
 <div className="p-4">
 {children}
-<p>endpoints you can use with your given token(remember it last for 5hours!. You can get a new one afterwards here. <Link href="/refresh">Refresh Token</Link>:
+<p>endpoints you can use with your given token:
 
 <ul>
 
 <li><Link href="/shop">E-Commerce</Link></li>
 
 </ul>
+remember your token last for 5 hours! You can get a new one afterwards here <Link href="/refresh">Refresh Token</Link>
+
+</p>
 </div>
-</>
 )
 }
 
