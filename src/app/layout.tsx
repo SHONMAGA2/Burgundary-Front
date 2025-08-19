7d9de7b199dc9fe861d7a51043eba4a153164fd0
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-08-19 20:09:14 +0200

    Update layout.tsx Added endpoints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import React from 'react';
+import Link from 'next/link's;
 
 export default function HomeLayout({children}:{children:React.ReactNode}){
 
 return(
 <>
 <div className="p-4">
-
 {children}
-</di>
+<p>endpoints you can use with your given token(remember it last for 5hours!. You can get a new one afterwards here. <Link href="/refresh">Refresh Token</Link>:
+
+<ul>
+
+<li><Link href="/shop">E-Commerce</Link></li>
+
+</ul>
+</div>
 </>
 )
 }
