753d89e684c8c3c55f69f34cf73cd9ef78ba5025
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-08-19 19:47:18 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,63 +1,15 @@
 "use client";
 
-import React,{useEffect,useState} from 'react';
-
-interface SignUp {
-message:string,
-token:string
-
-}
+import React from 'react';
 
 export default function HomeLayout({children}:{children:React.ReactNode}){
-
-const [data,setData] = useState<SignUp | null>(null);
-
-useEffect(() => {
-async function getData():Promise<void>{
-try{
-const request =
- await 
-fetch("https://burgundary-api-8dpz.onrender.com/api/SignUp",{
-method:"GET",
-credentials:"include"
-});
-
-if(request.ok){
-const data:SignUp = await request.json();
-setData(data);
-}
-
-}catch(error){
-
-if (error instanceof Error){
-console.error("Something went wrong",error.message);
-
-}
-else{
-console.error("Something went wrong",error);
-}
-}
-}
-
-getData();
-
-},[]);
 
 return(
 <>
 <div className="p-4">
 
 {children}
-<div className = " display">
-</div>
-
-{data && (
-<>
-<p>Account Created!, copy your token below</p>
-<pre>{data.token}</pre>
-</>
-)}
-</div>
+</di>
 </>
 )
 }
