036201711612a0258cab284b0570cf6a1395a7d9
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-10 22:44:48 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,20 @@ import Link from 'next/link';
 export default function HomeLayout({children}:{children:React.ReactNode}){
 
 return(
-<div className="p-4">
-{children}
-<p>endpoints you can use with your given token:
+    <div className="p-4">
+    {children}
+    <p>endpoints you can use with your given token:</p>
 
-<ul>
+    <ul>
 
-<li><Link href="/shop">E-Commerce</Link></li>
+    <li><Link href="/shop">E-Commerce</Link></li>
 
-</ul>
-remember your token last for 5 hours! You can get a new one afterwards here <Link href="/refresh">Refresh Token</Link>
+    </ul>
 
-</p>
-</div>
+    <p>remember your token last for 5 hours! You can get a new one afterwards here <Link href="/refresh">Refresh Token</Link>
+
+    </p>
+    </div>
 )
 }
 
